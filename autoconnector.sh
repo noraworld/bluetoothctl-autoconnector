12ad11eaf2aaf726c07de0f9f4262ee43cc3bf13
noraworld <mail@noraworld.com>
2021-01-03 08:53:23 +0000

    Attempt to connect all paired devices

--- autoconnector.sh
+++ autoconnector.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+function paired_devices() {
+  {
+    printf "select $adapter\n\n"
+    printf "paired-devices\n\n"
+  } | bluetoothctl | grep "Device " | sed -r 's/^.*(([0-9A-F]{2}:){5}[0-9A-F]{2}).*$/\1/'
+}
+
 # check if the device is being connected
 # return 'yes' or 'no'
 function is_connected() {
@@ -9,20 +16,35 @@ function is_connected() {
   } | bluetoothctl | grep "Connected: " | sed -e 's/Connected: //' | sed -e 's/^[[:blank:]]*//'
 }
 
-while read line
+bluetoothctl -- list | while read line
 do
-  # skip a comment line and a blank line
-  if ! [[ $line =~ ^# ]] && ! [[ $line =~ ^([[:blank:]]+.*)*$ ]]; then
-    # save Bluetooth adapter’s MAC address and device’s MAC address
-    adapter=`echo "$line," | cut -d ',' -f 1`
-    device=`echo "$line," | cut -d ',' -f 2`
+  adapter=`echo $line | sed -r 's/^.*(([0-9A-F]{2}:){5}[0-9A-F]{2}).*$/\1/'`
 
-    # connect the device if not connected
+  paired_devices | while read device
+  do
     if [[ $(is_connected) = "no" ]]; then
       {
         printf "select $adapter\n\n"
         printf "connect $device\n\n"
       } | bluetoothctl
     fi
-  fi
-done < $(dirname $0)/list
+  done
+done
+
+#while read line
+#do
+#  # skip a comment line and a blank line
+#  if ! [[ $line =~ ^# ]] && ! [[ $line =~ ^([[:blank:]]+.*)*$ ]]; then
+#    # save Bluetooth adapter’s MAC address and device’s MAC address
+#    adapter=`echo "$line," | cut -d ',' -f 1`
+#    device=`echo "$line," | cut -d ',' -f 2`
+#
+#    # connect the device if not connected
+#    if [[ $(is_connected) = "no" ]]; then
+#      {
+#        printf "select $adapter\n\n"
+#        printf "connect $device\n\n"
+#      } | bluetoothctl
+#    fi
+#  fi
+#done < $(dirname $0)/list
